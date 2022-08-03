630ee233236456a189741c1e9120bf7d92788f41
kfupm123 <kfupm.mxproject@gmail.com>
2022-08-04 00:19:16 +0500

    Latest

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const BASE_API = "http://157.175.97.218:8000/api";
+export const BASE_API = "https://obscure-stream-12511.herokuapp.com/api";
 export const MAPS_API_KEY = "AIzaSyBnO5tMfRvu3aQisyu0ujrAyiLdsCAb8gE";
